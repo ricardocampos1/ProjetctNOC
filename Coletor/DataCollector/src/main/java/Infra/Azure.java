e1000b893671cd9307b17ac2cc1b46ee5062d074
Ricardo Campos de Oliveira <37667683+ricardocampos1@users.noreply.github.com>
2018-11-03 12:52:55 -0300

    Altera forma de conexão com o banco

--- Coletor/DataCollector/src/main/java/Infra/Azure.java
+++ Coletor/DataCollector/src/main/java/Infra/Azure.java
@@ -2,59 +2,49 @@ package Infra;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Azure {
 
-    private final String connectionString = "jdbc:sqlserver://zackariel.database.windows.net:1433;database=Duffoux;user=duffoux@zackariel;password=Rick091295;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-    private String commandSQL;
+    private final String connectionString = "jdbc:sqlserver://pyxia.database.windows.net:1433;database=Pyxia;user=pyxia@pyxia;password=Admin@admin;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+    private Connection connection;
 
-    public String Connection(String typeStatement) {
+    public Connection getConnection() {
+        return connection;
+    }
+    
+    public Connection openConnection() throws SQLException {
         try (Connection conn = DriverManager.getConnection(connectionString)) {
-            this.commandSQL = typeStatement;
-            String detectType = typeStatement.substring(0, 6).toUpperCase();
-            if (null == detectType) {
-                conn.close();
-                return "Comando SQL não identificado, conexão com o banco fechada.";
-            } else {
-                switch (detectType) {
-                    case "SELECT":
-                        SelectCommand(conn);
-                        break;
-                    case "INSERT":
-                        InsertCommand(conn);
-                        break;
-                    default:
-                        conn.close();
-                        return "Comando SQL não identificado, conexão com o banco fechada.";
-                }
-            }
+            connection = conn;
+            System.out.println("Conexão estabelecida");
+            return conn;
+        }
+    }
+    
+    public void closeConnetion(Connection conn) throws SQLException{
+        if (conn.isClosed()) {
             conn.close();
-        } catch (SQLException e) {
-            return "Falha ao conectar com o banco\n" + e.getErrorCode();
-        }
-        return "Comando realizado com sucesso.";
-    }
-
-    private void SelectCommand(Connection conn) {
-        try (Statement statement = conn.createStatement();
-                ResultSet resultSet = statement.executeQuery(this.commandSQL)) {
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getErrorCode());
+            System.out.println("Conexão finalizada");
         }
     }
 
-    private void InsertCommand(Connection conn) {
-        try (Statement statement = conn.createStatement()) {
-            int rowsAffected = statement.executeUpdate(this.commandSQL);
-            System.out.println(rowsAffected + " linha(s) inseridas");
-        } catch (SQLException e) {
-            System.out.println(e.getErrorCode());
-        }
-    }
+//    private void SelectCommand(Connection conn) {
+//        try (Statement statement = conn.createStatement();
+//                ResultSet resultSet = statement.executeQuery(this.commandSQL)) {
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getInt(1));
+//            }
+//        } catch (SQLException e) {
+//            System.out.println(e.getErrorCode());
+//        }
+//    }
+//
+//    private void InsertCommand(Connection conn) {
+//        try (Statement statement = conn.createStatement()) {
+//            int rowsAffected = statement.executeUpdate(this.commandSQL);
+//            System.out.println(rowsAffected + " linha(s) inseridas");
+//        } catch (SQLException e) {
+//            System.out.println(e.getErrorCode());
+//        }
+//    }
 }
