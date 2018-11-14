a4b1ff89ee761337f0ca03360429b7cac83b7c22
Aluno Bandtec <github@alunos.bandtec.com.br>
2018-11-14 17:07:23 -0200

    Nova versão Dash em produçao

---
+++ Pyxia/Pyxia/Pyxia/Cadastro.aspx.designer.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Pyxia {
+    
+    
+    public partial class Cadastro {
+        
+        /// <summary>
+        /// Controle regForm.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm regForm;
+        
+        /// <summary>
+        /// Controle btnNext.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnNext;
+    }
+}
