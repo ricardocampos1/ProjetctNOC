c878eba4e8027adabc5b981a18901c4431b1baef
ProcG <gsilvaproc@gmail.com>
2018-12-02 16:41:37 -0200

    Adicionando Dropdown com SqlConnection

--- Site/Pyxia/Pyxia/dashboard.aspx.cs
+++ Site/Pyxia/Pyxia/dashboard.aspx.cs
@@ -14,7 +14,27 @@ namespace Pyxia
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-       
+            if (!this.IsPostBack)
+            {
+                using (SqlConnection conn = new SqlConnection("Server=tcp:pyxia.database.windows.net,1433;Initial Catalog=Pyxia;Persist Security Info=False;User ID=pyxia;Password=Admin@admin;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT id_machine, name_machine FROM tb_machine where id_user =" + Session["id_user"]))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = conn;
+                        using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                        {
+                            DataSet ds = new DataSet();
+                            sda.Fill(ds);
+                            ddlMachine.DataSource = ds.Tables[0];
+                            ddlMachine.DataTextField = "name_machine";
+                            ddlMachine.DataValueField = "id_machine";
+                            ddlMachine.DataBind();
+                        }
+                    }
+                }
+                ddlMachine.Items.Insert(0, new ListItem("--Selecione sua Maquina--", "0"));
+            }
         }
     }
 }
